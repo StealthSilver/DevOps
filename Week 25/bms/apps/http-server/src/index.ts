6f5767955ac926b5e7a71abae89516470179596f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-17 11:24:39 +0530

    http servrer

--- Week 25/bms/apps/http-server/src/index.ts
+++ Week 25/bms/apps/http-server/src/index.ts
@@ -8,9 +8,20 @@ app.get("/", (req, res) => {
   res.send("hi there");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  const user = await client.user.create({
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+
+  res.json({
+    message: "Signup successful",
+    id: user.id,
+  });
 });
 
 app.listen(3000);
