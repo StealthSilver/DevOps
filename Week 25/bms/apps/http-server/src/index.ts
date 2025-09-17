fcbe6b463b09c11b786450d3d5ad8d244a503772
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-17 11:22:04 +0530

    http servrer

--- Week 25/bms/apps/http-server/src/index.ts
+++ Week 25/bms/apps/http-server/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { client } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hi there");
+});
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 });
 
 app.listen(3000);
